7dcd49ed7db7da8cf5430aa1bcd0e32ec20817f7
nav <navis@DESKTOP-Q4F6ITI>
2017-05-22 00:12:01 -0700

    added hello world message.

--- NavView/Form1.cs
+++ NavView/Form1.cs
@@ -16,5 +16,10 @@ namespace NavView
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
     }
 }
